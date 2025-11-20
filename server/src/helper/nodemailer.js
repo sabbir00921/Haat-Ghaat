61c58974a4002af3b9b0f0c9ff95cc9f98a35afa
sabbir00921 <sabbir00921@gmail.com>
2025-11-20 17:32:39 +0600

    fix mailer v0.6

--- server/src/helper/nodemailer.js
+++ server/src/helper/nodemailer.js
@@ -1,33 +1,29 @@
 const nodemailer = require("nodemailer");
 const { customError } = require("../utils/customError");
 
-// Create a test account or replace with real credentials.
 const transporter = nodemailer.createTransport({
-  host: "gmail",
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
     user: process.env.HOST_MAIL,
     pass: process.env.APP_PASSWORD,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
-  logger: true, // ADD THIS
-  debug: true, // ADD THIS
+  logger: true,
+  debug: true,
 });
 
 exports.mailer = async (subject, template, email) => {
   try {
     const info = await transporter.sendMail({
-      from: '"VIRUS COMPUTER" <sabbir000921@gmail.com>',
+      from: `"VIRUS COMPUTER" <sabbir000921@gmail.com>`,
       to: email,
-      subject: subject,
+      subject,
       html: template,
     });
-    // console.log(info.messageId);
+
     return info;
   } catch (error) {
-    throw new customError(500, "Email sending failed" + error.message);
+    throw new customError(500, "Email sending failed: " + error.message);
   }
 };
